06f846051518bf41476dfc97cb78949a9e09a517
mpreetha99 <43870680+mpreetha99@users.noreply.github.com>
2023-03-18 19:31:49 -0400

    Update app.js

--- website/app.js
+++ website/app.js
@@ -77,3 +77,31 @@ function f10(e) {
 window.addEventListener('load', () => {
     textarea.value = "";
 });
+
+let saveFile = () => {
+
+    // Get the data from each element on the form.
+
+    const msg = document.getElementById('textarea1');
+
+    // This variable stores all the data.
+    let data = msg.value;
+
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+      newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+      newLink.href = window.URL.createObjectURL(textToBLOB);
+      newLink.style.display = "none";
+      document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
+  }
