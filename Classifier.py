4654d746b16cbe2152f7a4870cd19ad5d53f276a
La1xuan <shawnzhong18@gmail.com>
2023-03-18 17:28:18 -0400

    update

--- Classifier.py
+++ Classifier.py
@@ -7,22 +7,30 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
-print("asoduaho")
-categories = ['sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=False, random_state=42)
-twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=False, random_state=42)
+depressionText = pd.read_csv("dataset\\depression.csv")
+training = depressionText["post"].to_list()
+target = [0] * len(training)
 
-info = pd.read_csv("dataset\\depression.csv")
-result = info["post"].to_list()
+depressionText = pd.read_csv("dataset\\Tweets.csv")
+normalText = depressionText[["text", "sentiment"]]
+    
+print(normalText.shape)
+
+for i in range(normalText.shape[0]):
+    if normalText["sentiment"][i] != "negative":
+        #training.append(normalText["text"][i])
+        target.append(1)
+        print(normalText["text"][i])
+
 
 count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(result)
+X_train_counts = count_vect.fit_transform(training)
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
 print(X_train_tf.shape)
 
 tfidVect = TfidfVectorizer()
-X_train_direct = tfidVect.fit_transform(result)
+X_train_direct = tfidVect.fit_transform(training)
 
 print(X_train_direct.shape)
 
--- converter.py
+++ converter.py
@@ -1,12 +1,6 @@
 import pandas as pd
+print([1] * 1000)
 
-info = pd.read_csv("dataset\\depression.csv")
-output = []
-result = info[["post"]]
-for i in result:
-    output.append(result[i][1].replace("\n", "").replace(",", ""))
-    break
-print(output)
 #result.to_csv("dataset\\depression1.csv")
 
 
