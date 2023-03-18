8707f5830184ed57f9a879935337f43747c732b5
lveluesl <128001286+lveluesl@users.noreply.github.com>
2023-03-18 18:38:51 -0400

    Update Naive Bayes.py

--- Naive Bayes.py
+++ Naive Bayes.py
@@ -33,4 +33,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
 model = GaussianNB()
 model.fit(X_train.toarray(), y_train)
 print(model.score(X_test.toarray(), y_test))
-
+# 0.8310344827586207
